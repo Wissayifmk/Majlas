32f703a9c9c402bc28dc8ff30f37d0399c7b60eb
Jood203 <joud.m.h.44@gmail.com>
2024-03-20 21:07:49 +0300

    edit3

---
+++ ProjectDelete.php
@@ -0,0 +1,30 @@
+<?php
+$host="localhost";
+        $user="root";
+        $pass="root";
+        $database="majlas";
+        $connection= mysqli_connect($host,$user,$pass,$database);
+        $error= mysqli_error($connection);
+        if($error!=null)
+         {
+         $output="<p>can not connect to databsase</p>".$error;
+         exit($output);
+         } else 
+         {
+             if (isset($_GET['id']))
+             {
+            $projectID = $_GET['id'];
+            $deleteQuery = "DELETE FROM DesignPortoflioProject WHERE id = $projectID";
+            $deleteResult = mysqli_query($connection, $deleteQuery);
+             if ($deleteResult) 
+             {
+             header("Location: index.php"); //DesignerHomePage
+             exit();
+             } else {
+                     echo 'Error deleting the project from the database.';
+                    }
+             }
+         }
+
+
+         mysqli_close($connection);
