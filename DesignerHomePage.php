b1da586621c62afc550076587e981929908b79d1
Jood203 <joud.m.h.44@gmail.com>
2024-03-20 23:02:00 +0300

    edit11

--- DesignerHomePage.php
+++ DesignerHomePage.php
@@ -25,15 +25,14 @@
            $designerID = $_SESSION['id'];
            $query1 = "SELECT * FROM Designer WHERE id = $designerID";
            $result1 = mysqli_query($connection, $query1);
-           
          }
     ?>
     
     <body>  
         <header>
-        <img src="image2/tapImage.PNG" alt="Majlas's Logo" width="200">
+        <img src="image/tapImage.PNG" alt="Majlas's Logo" width="200">
             <button type="button" onclick="window.location.href = 'index.html'" class="log-out">
-                <img src="image2/Log-Out.png" alt="log-out">
+                <img src="image/Log-Out.png" alt="log-out">
             </button>    
         </header>
         <main>
@@ -49,7 +48,7 @@
                       $logoImgFileName = $designer['logoImgFileName'];
                     }
                    echo '<div class="designer-info">';
-                   echo '<h1>Welcome'.$firstName.'</h1>';
+                   echo '<h1>Welcome '.$firstName.'</h1>';
                    echo '<p>';
                    echo '<h3>Designer information:</h3>'; 
                    echo '<span>First name:</span> ' . $firstName . '<br>';
@@ -58,8 +57,8 @@
                            . $emailAddress . '</a><br>';
                    echo '<span>Brand Name:</span> ' . $brandName . '<br>';
                    
-                   $sql="SELECT * FROM DesignCategory WHERE id = $designerID";
-                   echo '<span>Category:</span> ' . $category . '<br>';
+               
+                   echo '<span>Category:</span> ' .$category. '<br>'; ///dosent show
                    echo '</p>';
                    echo '</div>';
                 ?>
@@ -76,19 +75,18 @@
                 </div>
             </div>
 
+             <table id="table1">
+                 <tr>
+                <th>Project Name</th>
+                <th>Image</th>
+                <th>Design Category</th>
+                <th>Description</th>
+                <th>Edit</th>
+                <th>Delete</th>
+                </tr>
             <?php 
             $query2 = "SELECT * FROM DesignPortoflioProject WHERE designerID = $designerID";
             $result2 = mysqli_query($connection, $query2);
-
-                echo '<table id="table1">';
-                echo '<tr>';
-                echo '<th>Project Name</th>';
-                echo '<th>Image</th>';
-                echo '<th>Design Category</th>';
-                echo '<th>Description</th>';
-                echo '<th>Edit</th>';
-                echo '<th>Delete</th>';
-                echo '</tr>';
             if ($result2 && mysqli_num_rows($result2) > 0) {
   
                while ($row = mysqli_fetch_assoc($result2)) {
@@ -109,22 +107,24 @@
                    echo '<td><a href="ProjectDelete.php?id=' . $projectID . '">Delete</a></td>';
                    echo '</tr>';
                     }
-                   echo '</table>';
+                   
             }
              ?>
+                </table>
 
             <section id="consultation-requests">
                 <h2 class="header1">Design Consultation Requests</h2>
+                <table>
+                 <tr>
+                  <th>Client</th>
+                  <th>Room</th>
+                 <th>Dimensions</th>
+                 <th>Design Category </th>
+                 <th>Color Preferences</th>
+                <th>Date</th>
+               </tr>
               <?php 
-                 echo '<table>';
-                  echo '<tr>';
-                  echo '<th>Client</th>';
-                  echo '<th>Room</th>';
-                  echo '<th>Dimensions</th>';
-                 echo '<th>Design Category </th>';
-                 echo '<th>Color Preferences</th>';
-                echo '<th>Date</th>';
-                echo '</tr>';
+                 
                 
                 $query3 = "SELECT dcr.id, c.firstName AS clientFName, c.lastName AS clientLName, rt.type AS roomType, dc.category AS designCategory, dcr.roomWidth, dcr.roomLength, dcr.colorPreferences, dcr.date
            FROM DesignConsultationRequest dcr
@@ -150,6 +150,7 @@
                         }
              
               ?>
+                </table>
             </section>
 
         </main>
