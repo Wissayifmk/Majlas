c17cbb3cd4c32c6050b8fdf3cb29b2ab3bbf0b69
walze <walze@DESKTOP-38GJ2B4>
2024-03-23 21:50:32 +0300

    LINA

--- DesignConsultation.php
+++ DesignConsultation.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (isset($SESSION['id']) && isset($SESSION['type'])) {
+if (isset($_SESSION['id']) && isset($_SESSION['type'])) {
     $DesignertID = $_SESSION['id'];
     $Type = $_SESSION['type'];
 }
