b5f7d9da0a2c93ee5f4227f8cabbc1f34dd87def
Jood203 <joud.m.h.44@gmail.com>
2024-03-24 12:48:26 +0300

    edit34

--- DesignerHomePage.php
+++ DesignerHomePage.php
@@ -14,26 +14,24 @@ require 'checkSecurity.php';
     </head>
 
     <?php
-        $host="localhost";
-        $user="root";
-        $pass="root";
-        $database="majlas";
-        $connection= mysqli_connect($host,$user,$pass,$database);
-        $error= mysqli_error($connection);
-        if($error!=null)
-         {
-         $output="<p>can not connect to databsase</p>".$error;
-         exit($output);
-         } else {
-           $query1 = "SELECT * FROM Designer WHERE id = $DesignerID";
-           $result1 = mysqli_query($connection, $query1);
-   
-         }
+    $host = "localhost";
+    $user = "root";
+    $pass = "root";
+    $database = "majlas";
+    $connection = mysqli_connect($host, $user, $pass, $database);
+    $error = mysqli_error($connection);
+    if ($error != null) {
+        $output = "<p>can not connect to databsase</p>" . $error;
+        exit($output);
+    } else {
+        $query1 = "SELECT * FROM Designer WHERE id = $DesignerID";
+        $result1 = mysqli_query($connection, $query1);
+    }
     ?>
-    
+
     <body>  
         <header>
-        <img src="image/tapImage.PNG" alt="Majlas's Logo" width="200">
+            <img src="image/tapImage.PNG" alt="Majlas's Logo" width="200">
             <button type="button"  onclick="window.location.href = 'SignOut.php'" class="log-out">
                 <img src="image/Log-Out.png" alt="log-out">
             </button>    
@@ -41,140 +39,172 @@ require 'checkSecurity.php';
         <main>
             <section class="part0">
                 <div class="headerContent">
-              <?php
-              if ($result1 && mysqli_num_rows($result1) > 0) {//check when log in 
-                  $designer = mysqli_fetch_assoc($result1);
-                  $firstName = $designer['firstName'];
-                  $lastName = $designer['lastName'];
-                  $emailAddress = $designer['emailAddress'];
-                   $brandName = $designer['brandName'];
-                   $logoImgFileName = $designer['logoImgFileName'];
+                    <?php
+                    if ($result1 && mysqli_num_rows($result1) > 0) {//check when log in 
+                        $designer = mysqli_fetch_assoc($result1);
+                        $firstName = $designer['firstName'];
+                        $lastName = $designer['lastName'];
+                        $emailAddress = $designer['emailAddress'];
+                        $brandName = $designer['brandName'];
+                        $logoImgFileName = $designer['logoImgFileName'];
 
-    echo '<div class="designer-info">';
-    echo '<h1>Welcome ' . $firstName . '</h1>';
-    echo '<p>';
-    echo '<h3>Designer information:</h3>';
-    echo '<span>First name:</span> ' . $firstName . '<br>';
-    echo '<span>Last name:</span> ' . $lastName . '<br>';
-    echo '<span>Email address:</span> <a href="mailto:' . $emailAddress . '">' . $emailAddress . '</a><br>';
-    echo '<span>Brand Name:</span> ' . $brandName . '<br>';
-    echo '<span>Category:</span>';
-    $query4 = "SELECT designCategoryID FROM DesignerSpeciality WHERE designerID=$DesignerID";
-    $result4 = mysqli_query($connection, $query4);
-    if ($result4) 
-    {
-        while ($row2 = mysqli_fetch_assoc($result4)) 
-        {
-            $catid = $row2['designCategoryID'];
-            $query5 = "SELECT category FROM DesignCategory WHERE id=$catid";
-            $result5 = mysqli_query($connection, $query5);
-            if ($result5) 
-            {
-                $row3 = mysqli_fetch_assoc($result5);
-                echo  $row3['category']." " ;
-            } else 
-            {
-                echo 'Error fetching design category: ' . mysqli_error($connection);
-            }
-        }
-        echo '</p>';
-       echo '</div>';
-    } else 
-    {
-        echo 'Error fetching designer specialties: ' . mysqli_error($connection);
-    }
-   
-} else {
-    echo 'No designer found.';
-}
-?>
+                        echo '<div class="designer-info">';
+                        echo '<h1>Welcome ' . $firstName . '</h1>';
+                        echo '<p>';
+                        echo '<h3>Designer information:</h3>';
+                        echo '<span>First name:</span> ' . $firstName . '<br>';
+                        echo '<span>Last name:</span> ' . $lastName . '<br>';
+                        echo '<span>Email address:</span> <a href="mailto:' . $emailAddress . '">' . $emailAddress . '</a><br>';
+                        echo '<span>Brand Name:</span> ' . $brandName . '<br>';
+                        echo '<span>Category:</span>';
+                        $query4 = "SELECT designCategoryID FROM DesignerSpeciality WHERE designerID=$DesignerID";
+                        $result4 = mysqli_query($connection, $query4);
+                        if ($result4) {
+                            while ($row2 = mysqli_fetch_assoc($result4)) {
+                                $catid = $row2['designCategoryID'];
+                                $query5 = "SELECT category FROM DesignCategory WHERE id=$catid";
+                                $result5 = mysqli_query($connection, $query5);
+                                if ($result5) {
+                                    $row3 = mysqli_fetch_assoc($result5);
+                                    echo $row3['category'] . " ";
+                                } else {
+                                    echo 'Error fetching design category: ' . mysqli_error($connection);
+                                }
+                            }
+                            echo '</p>';
+                            echo '</div>';
+                        } else {
+                            echo 'Error fetching designer specialties: ' . mysqli_error($connection);
+                        }
+                    } else {
+                        echo 'No designer found.';
+                    }
+                    ?>
                 </div>
                 <span class="imgHover">
-                    <?php  echo '<img src="image/' . $logoImgFileName . '" alt="designer logo" id="image0" width=80%>'; ?> 
+                    <?php echo '<img src="image/' . $logoImgFileName . '" alt="designer logo" id="image0" width=80%>'; ?> 
                 </span>
             </section>   
             <div class="desHeader">
                 <h3 class="header1">Design Portfolio</h3>
                 <div class="add">
-                    <?php echo '<a id="Add" href="ProjectAddition.php">Add New Project</a>';?>
-                    
+                    <?php echo '<a id="Add" href="ProjectAddition.php">Add New Project</a>'; ?>
+
                 </div>
             </div>
             <table id="table1">
-                 <tr>
-                <th>Project Name</th>
-                <th>Image</th>
-                <th>Design Category</th>
-                <th>Description</th>
-                <th>Edit</th>
-                <th>Delete</th>
+                <tr>
+                    <th>Project Name</th>
+                    <th>Image</th>
+                    <th>Design Category</th>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                 </tr>
-            <?php 
-            $query2 = "SELECT * FROM DesignPortoflioProject WHERE designerID = $DesignerID";
-            $result2 = mysqli_query($connection, $query2);
-            if ($result2 && mysqli_num_rows($result2) > 0) {
-  
-               while ($row = mysqli_fetch_assoc($result2)) {
-                    $projectID = $row['id'];
-                    $projectName = $row['projectName'];
-                    $imageFileName = $row['projectImgFileName'];
-                    $description = $row['description'];
-                    $designCategory = $row['designCategoryID'];
+                <?php
+                $query2 = "SELECT * FROM DesignPortoflioProject WHERE designerID = $DesignerID";
+                $result2 = mysqli_query($connection, $query2);
+                if ($result2 && mysqli_num_rows($result2) > 0) {
 
-                   echo '<tr>';
-                   echo '<td>' . $projectName . '</td>';
-                   echo '<td><img src="image/' . $imageFileName 
-                           . '" alt="project image" id="image' . $projectID . '" width=50%></td>';
-                   echo '<td>' . $designCategory . '</td>';
-                   echo '<td>' . $description . '</td>';
-                   
-                   echo '<td><a href="ProjectUpdate.php?id=' . $projectID . '">Edit</a></td>'; //name of the page + work or not
-                   echo '<td><a href="ProjectDelete.php?id=' . $projectID . '">Delete</a></td>';
-                   echo '</tr>';
+                    while ($row = mysqli_fetch_assoc($result2)) {
+                        $projectID = $row['id'];
+                        $projectName = $row['projectName'];
+                        $imageFileName = $row['projectImgFileName'];
+                        $description = $row['description'];
+                        $designCategory = $row['designCategoryID'];
+
+                        echo '<tr>';
+                        echo '<td>' . $projectName . '</td>';
+                        echo '<td><img src="image/' . $imageFileName
+                        . '" alt="project image" id="image' . $projectID . '" width=50%></td>';
+                        echo '<td>' . $designCategory . '</td>';
+                        echo '<td>' . $description . '</td>';
+
+                        echo '<td><a href="ProjectUpdate.php?id=' . $projectID . '">Edit</a></td>'; //name of the page + work or not
+                        echo '<td><a href="ProjectDelete.php?id=' . $projectID . '">Delete</a></td>';
+                        echo '</tr>';
                     }
-                   
-            }
-             ?>
-                </table>
+                }
+                ?>
+            </table>
 
             <section id="consultation-requests">
                 <h2 class="header1">Design Consultation Requests</h2>
                 <table>
-                 <tr>
-                  <th>Client</th>
-                  <th>Room</th>
-                 <th>Dimensions</th>
-                 <th>Design Category </th>
-                 <th>Color Preferences</th>
-                <th>Date</th>
-               </tr>
-              <?php 
-                 
-                //check the query
-                $query3 = "SELECT dcr.id, c.firstName AS clientFName, c.lastName AS clientLName, rt.type AS roomType, dc.category AS designCategory, dcr.roomWidth, dcr.roomLength, dcr.colorPreferences, dcr.date
-           FROM DesignConsultationRequest dcr
-           INNER JOIN Client c ON dcr.clientID = c.id
-           INNER JOIN RoomType rt ON dcr.roomTypeID = rt.id
-           INNER JOIN DesignCategory dc ON dcr.designCategoryID = dc.id
-           WHERE dcr.statusID = (SELECT id FROM RequestStatus WHERE status = 'pending consultation')
-           AND dcr.designerID = <designerID>";
-                
-                $result3 = mysqli_query($connection, $query3);
-                        while ($row = mysqli_fetch_assoc($result3)) {
-                            echo '<tr>';
-                            echo '<td>' . $row["clientFName"].' '.$row["clientLName"]. '</td>';
-                            echo '<td>' . $row["roomType"] . '</td>';
-                            echo '<td>' . $row["roomWidth"] . 'x' . $row["roomLength"] . '</td>';
-                            echo '<td>' . $row["designCategory"] . '</td>';
-                            echo '<td>' . $row["colorPreferences"] . '</td>';
-                            echo '<td>' . $row["date"] . '</td>';
-                            echo '<td><a href="DesignConsultation.php?id=' . $row['id'] . '">Provide Consultation</a></td>';
-                            echo '<td><a href="DeclineConsultation.php?id=' . $row['id'] . '">Decline Consultation</a></td>';
-                            echo '</tr>';
-                            
+                    <tr>
+                        <th>Client</th>
+                        <th>Room</th>
+                        <th>Dimensions</th>
+                        <th>Design Category </th>
+                        <th>Color Preferences</th>
+                        <th>Date</th>
+                    </tr>
+                    <?php
+                    
+                    $query3 = "SELECT id, clientID, roomTypeID, designCategoryID, roomWidth, roomLength, colorPreferences, date
+           FROM DesignConsultationRequest
+           WHERE statusID = (SELECT id FROM RequestStatus WHERE status = 'pending consultation')
+           AND designerID = <designerID>";
+                    $result3 = mysqli_query($connection, $query3);
+
+
+                    $queryClient = "SELECT id, firstName, lastName
+                FROM Client";
+                    $resultClient = mysqli_query($connection, $queryClient);
+
+
+                    $queryRoomType = "SELECT id, type
+                  FROM RoomType";
+                    $resultRoomType = mysqli_query($connection, $queryRoomType);
+
+
+                    $queryDesignCategory = "SELECT id, category
+                        FROM DesignCategory";
+                    $resultDesignCategory = mysqli_query($connection, $queryDesignCategory);
+
+
+                    while ($row = mysqli_fetch_assoc($result3)) {
+                        echo '<tr>';
+                        $clientID = $row["clientID"];
+                        $roomTypeID = $row["roomTypeID"];
+                        $designCategoryID = $row["designCategoryID"];
+
+                        
+                        mysqli_data_seek($resultClient, 0);
+                        while ($clientRow = mysqli_fetch_assoc($resultClient)) {
+                            if ($clientRow["id"] == $clientID) {
+                                echo '<td>' . $clientRow["firstName"] . ' ' . $clientRow["lastName"] . '</td>';
+                                break;
+                            }
                         }
-             
-              ?>
+
+                        
+                        mysqli_data_seek($resultRoomType, 0);
+                        while ($roomTypeRow = mysqli_fetch_assoc($resultRoomType)) {
+                            if ($roomTypeRow["id"] == $roomTypeID) {
+                                echo '<td>' . $roomTypeRow["type"] . '</td>';
+                                break;
+                            }
+                        }
+
+                        echo '<td>' . $row["roomWidth"] . 'x' . $row["roomLength"] . '</td>';
+
+                        
+                        mysqli_data_seek($resultDesignCategory, 0);
+                        while ($designCategoryRow = mysqli_fetch_assoc($resultDesignCategory)) {
+                            if ($designCategoryRow["id"] == $designCategoryID) {
+                                echo '<td>' . $designCategoryRow["category"] . '</td>';
+                                break;
+                            }
+                        }
+
+                        echo '<td>' . $row["colorPreferences"] . '</td>';
+                        echo '<td>' . $row["date"] . '</td>';
+                        echo '<td><a href="DesignConsultation.php?id=' . $row['id'] . '">Provide Consultation</a></td>';
+                        echo '<td><a href="DeclineConsultation.php?id=' . $row['id'] . '">Decline Consultation</a></td>';
+                        echo '</tr>';
+                    }
+                    ?>
                 </table>
             </section>
 
