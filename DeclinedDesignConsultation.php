da632069ca0bbf44fc8ec22865bd991cfac9f5bb
Jood203 <joud.m.h.44@gmail.com>
2024-03-31 20:05:53 +0300

    edit40

--- DeclinedDesignConsultation.php
+++ DeclinedDesignConsultation.php
@@ -1,23 +1,26 @@
 <?php
-$host="localhost";
-        $user="root";
-        $pass="root";
-        $database="majlas";
-        $connection= mysqli_connect($host,$user,$pass,$database);
-        $error= mysqli_error($connection);
-        if($error!=null)
-         {
-         $output="<p>can not connect to databsase</p>".$error;
-         exit($output);
-         } else 
-       {
-       // $query = "UPDATE DesignConsultationRequest SET statusID = (SELECT id FROM RequestStatus WHERE status = 'consultation declined') WHERE id = $requestID";
-         $result = mysqli_query($connection, $query);
-         if ($result) 
-         {
-             header("Location: designer_homepage.php");
-             exit();
-             
-         }
-        } 
-        mysqli_close($connection);
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$database = "majlas";
+$connection = mysqli_connect($host, $user, $pass, $database);
+$error = mysqli_error($connection);
+
+if ($error != null) {
+    $output = "<p>Cannot connect to the database</p>" . $error;
+    exit($output);
+} else {
+    // Retrieve the request ID from the URL
+    $requestID = $_GET['id'];
+
+    // Update the status of the consultation request
+    $query = "UPDATE designconsultationrequest SET statusID = (SELECT id FROM requeststatus WHERE status = 'consultation declined') WHERE id = $requestID";
+    $result = mysqli_query($connection, $query);
+
+    if ($result) {
+        $header = header("Location: DesignerHomePage.php");
+        exit();
+    }
+}
+
+mysqli_close($connection);
