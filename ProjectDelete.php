3f47ca411955e4d3b0da02948db938c4c2eaa402
Jood203 <joud.m.h.44@gmail.com>
2024-04-26 19:42:14 +0300

    edit58

--- ProjectDelete.php
+++ ProjectDelete.php
@@ -1,30 +1,25 @@
 <?php
-$host="localhost";
-        $user="root";
-        $pass="root";
-        $database="majlas";
-        $connection= mysqli_connect($host,$user,$pass,$database);
-        $error= mysqli_error($connection);
-        if($error!=null)
-         {
-         $output="<p>can not connect to databsase</p>".$error;
-         exit($output);
-         } else 
-         {
-             if (isset($_GET['id']))
-             {
-            $projectID = $_GET['id'];
-            $deleteQuery = "DELETE FROM DesignPortoflioProject WHERE id = $projectID";
-            $deleteResult = mysqli_query($connection, $deleteQuery);
-             if ($deleteResult) 
-             {
-             header("Location: DesignerHomePage.php"); 
-             exit();
-             } else {
-                     echo 'Error deleting the project from the database.';
-                    }
-             }
-         }
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$database = "majlas";
+$connection = mysqli_connect($host, $user, $pass, $database);
+$error = mysqli_error($connection);
 
+if ($error != null) {
+    $output = "<p>can not connect to database</p>" . $error;
+    exit($output);
+} else {
+    if (isset($_GET['id'])) {
+        $projectID = $_GET['id'];
+        $deleteQuery = "DELETE FROM DesignPortoflioProject WHERE id = $projectID";
+        $deleteResult = mysqli_query($connection, $deleteQuery);
+        if ($deleteResult) {
+            echo json_encode(["success" => true]);
+        } else {
+            echo json_encode(["success" => false]);
+        }
+    }
+}
 
-         mysqli_close($connection);
+mysqli_close($connection);
